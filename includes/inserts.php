b02f769932409d48bb86a9405a81614b3c779bf4
SimchaGemach <s0527656988@gmail.com>
2017-04-30 21:57:08 +0300

    cleaning inserts

--- includes/inserts.php
+++ includes/inserts.php
@@ -12,7 +12,8 @@ defined('DB_SERVER') ? null : define("DB_SERVER", "localhost");
 defined('DB_USER') ? null :define("DB_USER", "root");
 defined('DB_PASS') ? null :define("DB_PASS", "GemachSimcha");
 defined('DB_NAME') ? null :define("DB_NAME", "gmach");
-
+    
+    // CREATE DB if does not exist
     $new_database = new mysqli(DB_SERVER, DB_USER, DB_PASS);
     $query_file = LIB_PATH.DS.'sql_file.txt';
     $query_file_open = fopen($query_file, 'r');
@@ -21,17 +22,21 @@ defined('DB_NAME') ? null :define("DB_NAME", "gmach");
     $new_database->multi_query($sql);
     mysqli_close($new_database);
 
-   
+// Database connection
 $mysqli = new mysqli(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
-$balance_query = "SELECT SUM(Amount) as balance FROM transactions";
-$balance = $mysqli->query($balance_query);
-$balanc = mysqli_fetch_assoc($balance);
-$balance->close();
 $mysqli->set_charset("utf8");
 
+// balance of all loans, deposits, repayments, withdrawals and donations 
+    $balance_query = "SELECT SUM(Amount) as balance FROM transactions";
+    $balance = $mysqli->query($balance_query);
+    $balanc = mysqli_fetch_assoc($balance);
+    $balance->close();
+
+// variables for insert statements
 foreach ($_POST as $key => $value) {
     $$key = $value;
 }     
+
 if (isset($_POST['newloaner_submit'])) {   // if NEWLOANER tab is submitted
         
 
